1a09332ba821c5c638e2f6091fa719ca0856a770
umbacos <umbacos@gmail.com>
2020-11-06 23:44:36 +0100

    Update __init__.py

--- octoprint_uTempFanRelay/__init__.py
+++ octoprint_uTempFanRelay/__init__.py
@@ -112,7 +112,7 @@ class UtempfanrelayPlugin(octoprint.plugin.StartupPlugin,
             self.updateLCD()
 
     def hook_gcode_sending(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-        self._logger.info(comm_instance + " " + phase + " " + cmd)
+        self._logger.info(comm_instance + " " + cmd)
 #            if gcode == "M117":
 #                self.lcdTextBase = cmd
 
